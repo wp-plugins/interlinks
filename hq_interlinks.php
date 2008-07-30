b13883520b69b2cadfa29f9c61fbee0778ec607f
harleyquinedotcom <harleyquinedotcom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-30 19:47:30 +0000

    First version of Interlinks
    git-svn-id: https://plugins.svn.wordpress.org/interlinks/trunk@57011 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hq_interlinks.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Plugin Name: Interlinks
+Plugin URI: http://www.harleyquine.com/php-scripts/interlinks/
+Description: Make internal links in your blog simply by putting [[ and ]] (wikistyle) around the post title.
+Version: 2.0
+Author: Harley Quine
+Author URI: http://www.harleyquine.com
+*/
+
+/*  Copyright 2008  Lisa-Marie Welsh  (email : harley@harleyquine.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+add_filter('the_content', 'hq_interparse');
+add_filter('the_excerpt', 'hq_interparse');
+
+function hq_interreplace($val){
+
+      global $table_prefix, $wpdb, $user_ID;
+      $table_name = $table_prefix . "posts";
+      $val = addslashes($val);
+
+      $post_id = $wpdb->get_var("SELECT ID FROM $table_name WHERE post_title = '$val' AND post_status='publish'");
+      if(!$post_id){ return 0; }
+      else { return $post_id; }
+}
+
+function hq_interparse($content){
+   if(strpos($content, "[[")){
+      preg_match_all('/(\[\[.+?\]\])/',$content,$wikilinks, PREG_SET_ORDER);
+      foreach ($wikilinks as $val) {
+         if(strpos($val[0], "|")){
+                                 $pieces = explode("|", $val[0]);
+                                 $new_val = preg_replace('/\[\[(.+?)/', '$1', $pieces[0]);
+                                 $link_text = preg_replace('/(.+?)\]\]/', '$1', $pieces[1]);
+                                 $post_id = hq_interreplace($new_val);
+                                 if($post_id == 0){ $content2 = "<span style='color:red;'>$val</span>"; }
+                                 $permalink = get_permalink($post_id);
+                                 if($post_id){ $content2 = "<a href='$permalink'>$link_text</a>"; }
+                                 $content = str_replace($val, $content2, $content);
+                              }
+         else {
+                                 $new_val = preg_replace('/\[\[(.+?)\]\]/', '$1', $val[0]);
+                                 $post_id = hq_interreplace($new_val);
+                                 if($post_id == 0){ $content2 = "<span style='color:red;'>$val[0]</span>"; }
+                                 $permalink = get_permalink($post_id);
+                                 if($post_id){ $content2 = "<a href='$permalink'>$new_val</a>"; }
+                                 $content = str_replace($val, $content2, $content);
+                              }
+      }
+   }
+
+   return $content;
+   }
+
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Interlinks ===
+Contributors: harleyquinedotcom, outlyernet
+Donate link: http://www.harleyquine.com/
+Tags: plugin, linking, wiki
+Requires at least: ALL
+Tested up to: 2.6
+Stable tag: trunk
+
+Wikistyle links in your WordPress blog.
+
+== Description ==
+
+Create internal Wiki-style links in your WordPress blog. Easy to use and once you've started you won't be able to live without it. Simply put [[ and ]] around a post title and it will link to the post in your blog. Also supports [[Post Title|This is a link to a post]] style tags.
+
+This newest version fixes a bug with apostrophes in post titles. It also allows the more complicated linking style.
+
+== Installation ==
+
+1. Upload `hq-interlinks.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= How do I use it? =
+
+Simply put [[ and ]] around a post title within your post or excerpt and Interlinks will turn it into a link for you. You can also use the more advanced wiki style [[Post Title|This is a link to a post]]. If a link is red then the page doesn't exist.
+
+= Where can I get more information? =
+
+From the plugins page on http://www.harleyquine.com.
+
+== Screenshots ==
+
+1. Automatic ad placement locations.
+2. The config page.
+3. The author advertising user page.
+
+== Upgrading ==
+
+Simply delete the old one, upload and activate. Note that the file name has changed so delete kd_interlinks.php and upload hq-interlinks.php.
